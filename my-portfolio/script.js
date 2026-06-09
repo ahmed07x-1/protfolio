6514b915b0db3766c6b36a283e82f8ce2aed06ad
Mohd Younus <mohdyounus2004@gmail.com>
2026-06-09 10:10:39 +0530

    new

---
+++ my-portfolio/script.js
@@ -0,0 +1,99 @@
+const roles = [
+    "Full Stack Java Developer",
+    "Web Developer",
+    "Problem Solver",
+    "Java Learner"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const roleElement = document.querySelector(".hero-text h2");
+
+function typeEffect() {
+
+    const currentRole = roles[roleIndex];
+
+    if (!isDeleting) {
+
+        roleElement.textContent =
+            currentRole.substring(0, charIndex + 1);
+
+        charIndex++;
+
+        if (charIndex === currentRole.length) {
+
+            isDeleting = true;
+
+            setTimeout(typeEffect, 1200);
+
+            return;
+        }
+
+    } else {
+
+        roleElement.textContent =
+            currentRole.substring(0, charIndex - 1);
+
+        charIndex--;
+
+        if (charIndex === 0) {
+
+            isDeleting = false;
+
+            roleIndex =
+                (roleIndex + 1) % roles.length;
+        }
+    }
+
+    setTimeout(typeEffect,
+        isDeleting ? 50 : 100);
+}
+
+typeEffect();
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+sections.forEach((section) => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop - 200) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+
+        if (
+            link.getAttribute("href") === "#" + current
+        ) {
+            link.classList.add("active");
+        }
+    });
+});
